6253b989bb86bc8fb46cb10d25c802aa1b2c32e7
supergui <supergui@live.cn>
2023-08-08 15:16:36 +0800

    feat: Value unmarshal

--- value.go
+++ value.go
@@ -1,4 +1,21 @@
 package olayc
 
-// Value represents a configure value, it can be any type.
+import (
+	"github.com/pkg/errors"
+	yaml "gopkg.in/yaml.v2"
+)
+
+// Value represents a configure value, it can be scalar node or sub-tree node.
 type Value any
+
+// Unmarshal is implemented by using yaml utility,
+// value is firstly marshalled to yaml bytes,
+// then the yaml bytes is unmarshal to target out.
+// Thus, if 'out' is a struct, you must use the yaml struct tag.
+func Unmarshal(v Value, out any) error {
+	data, err := yaml.Marshal(v)
+	if err != nil {
+		return errors.Wrap(err, "Value.Unmarshal fail")
+	}
+	return yaml.Unmarshal(data, out)
+}
